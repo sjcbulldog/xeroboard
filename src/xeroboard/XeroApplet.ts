1c55181cf7e620c67234f6437c19bc864eecb49e
Jack (Butch) Griffin <butchg@comcast.net>
2023-10-23 10:38:55 -0700

    added support for rest requests

--- src/xeroboard/XeroApplet.ts
+++ src/xeroboard/XeroApplet.ts
@@ -3,17 +3,21 @@ import path from 'path' ;
 import { NTClient } from '../nt4/NTClient';
 import { BrowserWindow } from 'electron';
 
+export interface RestHandler { (req: Request<{}, any, any, any, Record<string, any>>, res: Response<any, Record<string, any>>): void }
+
 export class XeroApplet {
     private name: string ;
     private nettable: NTClient | null ;
     private server: express | null ;
     private window: BrowserWindow | null ;
+    private handlers: Map<string, RestHandler> ;
 
     public constructor(name: string) {
         this.nettable = null ;
         this.server = null ;
         this.window = null ;
         this.name = name ;
+        this.handlers = new Map<string, RestHandler>() ;
     }
 
     public start(nettable: NTClient, server: express) {
@@ -51,8 +55,37 @@ export class XeroApplet {
         return path.join(__dirname, '..', '..', 'content', 'applets', this.getName());
     }
 
+    protected registerRestRequest(req: string, handler: RestHandler) {
+        this.handlers.set(req, handler) ;
+    }
+
+    private restRequest(req, res) {
+        let prefix : string = '/' + this.getName() + '/rest/' ;
+        let key : string = req.path ;
+
+        if (key.startsWith(prefix)) {
+            key = key.substring(prefix.length) ;
+        }
+
+        let handler : RestHandler | undefined = this.handlers.get(key) ;
+        if (handler) {
+            handler(req, res) ;
+        }
+        else {
+            let obj = { 
+                error: "unknown rest request '" + req.path + "'"
+            } ;             
+            res.json(obj) ;
+        }
+    }
+
     private setupServerPaths() : void {
-        const name = '/' + this.getName() + '/*' ;
+        let name = '/' + this.getName() + '/rest/*' ;
+        this.server.get(name, (req, res) => {
+            this.restRequest(req, res) ;
+        }) ;
+
+        name = '/' + this.getName() + '/*' ;
         this.server.get(name, (req, res) => {
             let prefix: string = '/' + this.getName() ;
             let url:string = req.url ;
